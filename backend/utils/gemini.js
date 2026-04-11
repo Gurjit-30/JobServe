c5a3fd81c0c1339eeb83a163963caf3de1ed9338
Gurjit-30 <143714757+Gurjit-30@users.noreply.github.com>
2026-04-11 11:12:32 +0530

    modified:   backend/utils/gemini.js

--- backend/utils/gemini.js
+++ backend/utils/gemini.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("AIzaSyDzZm7lrEijyAwxM91tAG9Uo60HveAoTbo");
+const genAI = new GoogleGenerativeAI("AIzaSyD9PCYxrnNm9Oqbs0ju093vVXLHdCPRZOc");
 
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
